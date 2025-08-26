29ef47dc7b2d1218347e4e55ae4a65fa4512920b
Ethan Nicolas Cardenas Luna <cle0033245@est.univalle.edu>
2025-08-26 16:27:16 -0400

    Sprint 2, Incorporacion de Componentes y pagina de editar, mecanica de borrar

---
+++ BlazorApp.Client/Services/RepresentanteService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp.Entities;
+namespace BlazorApp.Client.Services
+{
+    public class RepresentanteService
+    {
+        private List<RepresentanteList> Lista;
+        public RepresentanteService()
+        {
+            Lista = new List<RepresentanteList>();
+            Lista.Add(new RepresentanteList { Num_Empl = 1, Nombre = "Juan Perez" });
+            Lista.Add(new RepresentanteList { Num_Empl = 2, Nombre = "Maria Gomez" });
+            Lista.Add(new RepresentanteList { Num_Empl = 3, Nombre = "Carlos Sanchez" });
+        }
+        public List<RepresentanteList> listarRepresentantes()
+        {
+            return Lista;
+        }
+    }
+}
+
+
---
+++ BlazorApp.Entities/RepresentanteList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Entities
+{
+    public class RepresentanteList
+    {
+        public int Num_Empl { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+}
